5ce1cd82053b5a0964482699119573678dc36b16
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-10-01 18:11:35 +0000

    updated to reflect topics 1.9, 1.10, 1.11

--- Main.java
+++ Main.java
@@ -1,61 +1,65 @@
 public class Main {
 
-    public static void main(String []args) {
+	public static void main(String[] args) {
 
-		// *** USING OBJECT METHODS ***
-        System.out.println("🏦 creating BankAccount objects 🏦"); 
+		// PART A: Calling static Methods
+		System.out.println("\n--- STATIC METHODS ---");
 
-		// HINT: check out the BankAccount.java file included in this project!
-        BankAccount rich = new BankAccount(90.99);
-		BankAccount broke = new BankAccount();
-		BankAccount lizard = new BankAccount("Lord Zuck", 106100000000.00);
+		// BIG QUESTION: Where do these METHODS come from? (where are they DEFINED?)
 
-		double richBal = rich.getBalance();
-        System.out.println("First account's balance: $" + richBal);
+		double accountBalance1 = 1500.00;
+		greet();
+		withdraw(accountBalance1);
 
-		// HINT: line 16 does the same thing as lines 12-13 in a different way...
-        System.out.println("Second account's balance: $" + broke.getBalance());
-		
-		System.out.println("In need of a cash infusion? Here ya go! 💸");
-        broke.deposit(100000);
-        System.out.println("UPDATED second account's balance: $" + broke.getBalance());
+		double accountBalance2 = 465.00;
+		greet();
+		withdraw(accountBalance2, 50.00);
 
-		System.out.println("The third account in our system seems sus 🦎 checking it out...");
-		System.out.println("Client's Name: " + lizard.getClientName());
+		double accountBalance3 = 789.00;
+		greet();
+		double amountToWithdraw = 35.00;
+		withdraw(accountBalance3, amountToWithdraw);
 
-		System.out.println("🔎 Investigating...");
-		
-		String realName = "Mark Zuckerburg";
-		lizard.setClientName(realName);
-		System.out.println("Client's REAL Name: " + lizard.getClientName());
-		
-        System.out.println("Time to redistribute some wealth #RobinHood");
-		
-        rich.withdraw(85000000000.00);
-		System.out.println("We solved world hunger! How much does Zuck have left? ");
-        System.out.println(lizard.getBalance());
+		// PART B: Using Math Methods
+		System.out.println("\n--- MATH METHODS ---");
 
-		// *** MATH CLASS METHODS ***
-      	System.out.println("\n--- MATH CLASS ---");
+		// BIG QUESTION: Where do these METHODS come from? (where are they DEFINED?)
 
-      	// HINT: try running the program multiple times...
-      	double rand = Math.random();
-      	System.out.println("Here's a random number: " + rand);
-      	double biggerRand = Math.random() * 500;
-     	System.out.println("Here's a bigger random number: " + biggerRand);
-      	int randInt = (int) (Math.random() * 100 + 1);
-      	System.out.println("Here's a random whole number: " + randInt);
+		double absoluteD = Math.abs(-3.14);
+		System.out.println("Absolute value of -3.14 is: " + absoluteD);
 
-      	double absoluteD = Math.abs(-3.14);
-      	System.out.println("Absolute value of -3.14 is: " + absoluteD);
-      	int absoluteI = Math.abs(-3);
-      	System.out.println("Absolute value of -3 is: " + absoluteI);
+		int absoluteI = Math.abs(-3);
+		System.out.println("Absolute value of -3 is: " + absoluteI);
 
-        double cubed = Math.pow(randInt, 3);
-        System.out.println(randInt + " to the 3rd power is: " + cubed);
+		double squareRoot = Math.sqrt(2500);
+		System.out.println("Square root of 2500 is: " + squareRoot);
 
-        double squareRoot = Math.sqrt(2500);
-        System.out.println("Square root of 2500 is: " + squareRoot);
+		// HINT: Try running the program multiple times to help explain this section...
+		double rand = Math.random();
+		System.out.println("Here's a random number: " + rand);
 
-   }
-}
+		double biggerRand = Math.random() * 500;
+		System.out.println("Here's a bigger random number: " + biggerRand);
+
+		int randInt = (int) (Math.random() * 100 + 1);
+		System.out.println("Here's a random whole number: " + randInt);
+
+		double cubed = Math.pow(randInt, 3);
+		System.out.println(randInt + " to the 3rd power is: " + cubed);
+
+	} // END of main method
+
+	public static void greet() {
+		System.out.println("🏧 Welcome to the ATM... please insert your card 💳");
+	}
+
+	public static void withdraw(double balance) {
+		System.out.println("You took out $" + balance + " leaving you with a balance of $0.00");
+	}
+
+	public static void withdraw(double balance, double amount) {
+		double newBalance = balance - amount;
+		System.out.println("You took out $" + amount + " leaving you with a balance of $" + newBalance);
+	}
+
+} // END of class
